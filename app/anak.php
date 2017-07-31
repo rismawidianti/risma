8e899145c0c2641a1a4154692d7a851c8b425aad
wahyu909 <151610097@smkassalaambandung.sch.id>
2017-07-31 16:35:46 +0700

    Belajar CRUD

---
+++ app/anak.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class anak extends Model
+{
+    //
+     protected $table = 'anaks';
+     protected $fillable =['nama','orangtua_id','umur','alamat'];
+     protected $visible  =['nama','orangtua_id','umur','alamat'];
+     public $timestamps =true;
+
+   public function orangtua()
+   {
+   	return $this->belongsTo('App\orangtua','orangtua_id');
+   }
+}
